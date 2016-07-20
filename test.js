406ec26d27c9952fb31af798135f49b91b7c890c
David Bjorklund <david.bjorklund@gmail.com>
2016-07-20 12:29:56 -0500

    basic functionality

---
+++ test.js
@@ -0,0 +1,41 @@
+import test from 'tapava';
+import cached from './lib';
+
+test('simple', async t => {
+  const c = cached(() => Promise.resolve(123));
+  const actual = await c();
+  const expected = 123;
+
+  t.is(actual, expected);
+});
+
+test('series', async t => {
+  let called = 0;
+  const c = cached(() => {
+    called++;
+    return Promise.resolve(123);
+  });
+  await c();
+  const actual = await c();
+  const expected = 123;
+
+  t.is(actual, expected);
+  t.is(called, 1);
+});
+
+test('concurrent', async t => {
+  let called = 0;
+  const c = cached(() => {
+    called++;
+    return new Promise(resolve => {
+      setTimeout(() => resolve(123), 5);
+    });
+  });
+  const actual = await Promise.all([
+    c(), c()
+  ]);
+  const expected = [123, 123];
+
+  t.deepEqual(actual, expected);
+  t.is(called, 1);
+});
